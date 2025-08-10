51bde6630c38375d96a214c437f3f048f04f04f7
Famitha M <145793075+Famitha-M@users.noreply.github.com>
2025-08-10 21:27:08 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,23 @@
+# Write your code below this line 👇
+# Printing Hello World in 3 Lines
+# Method 1
+print("Hello world!")
+print("Hello World!")
+print("Hello World!")
+#Method 2 - using \n new line
+print("Hello World!\nHello World!\nHello World!")
+
+#Printing two variables using + operator
+#Method 1
+print("Hello"+"World")
+#Method 2
+print("Hello "+"World")
+#Method 3
+print("Hello"+" World")
+#Method 4
+print("Hello"+" "+"World")
+
+
+
+
+
