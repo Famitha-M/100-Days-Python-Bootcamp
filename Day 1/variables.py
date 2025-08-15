fb82e4dfef93e378b8196ca7b24ff24a0bf4e44c
Famitha M <145793075+Famitha-M@users.noreply.github.com>
2025-08-15 23:09:03 +0530

    variables.py

---
+++ Day 1/variables.py
@@ -0,0 +1,14 @@
+# Declaring a Variable and Printing
+name=input("What is your name?")
+print(name)
+# Assigning a Value to a Variable and Printing
+name="Fami"
+print(name)
+# Printing a Length of the Variable
+print(len(name))
+# Printing Length of a variable using input function
+print(len(input("what is your name")))
+# Using Variables to print the above
+username=input("what is your name?")
+length=len(username)
+print(length)
