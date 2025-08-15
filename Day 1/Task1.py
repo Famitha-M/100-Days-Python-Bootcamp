f7b9c999a974741fd5d808e6f796efc436fed77a
Famitha M <145793075+Famitha-M@users.noreply.github.com>
2025-08-15 23:09:45 +0530

    Task1.py

---
+++ Day 1/Task1.py
@@ -0,0 +1,7 @@
+# CREATING A MUSIC BAND NAME USING PYTHON
+print("Welcome to Generate your Musical Band Names")
+print("Ready to get your dream band names!!!")
+print("hit the RUN button to get your names")
+genre=input("What is your Favorite Genre of Music?")
+creature=input("What is your Favorite Mythical Creature?")
+print(genre+" "+creature)
