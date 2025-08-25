2a4bb32fa9e722db1f48265b09714acf8d380942
Famitha M <145793075+Famitha-M@users.noreply.github.com>
2025-08-25 12:35:58 +0530

    type_error.py

---
+++ DAY 2/type_error.py
@@ -0,0 +1,17 @@
+# Type Checking using the term type
+print(type("Hello"))
+a=123
+print(type(a))
+b=3.1414
+print(type(b))
+c=True
+print(type(c))
+
+# Type conversion
+print(int("123")+int("456"))
+
+username=(input("Enter your name"))
+length_of_username=len(username)
+print(type("Number of letters in your name: "))
+print(type(length_of_username))
+print("Number of letters in your name: " + str(length_of_username))
