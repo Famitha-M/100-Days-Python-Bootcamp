a8f86847aa00dad07a866d30655587cfb88af3b8
Famitha M <145793075+Famitha-M@users.noreply.github.com>
2025-08-25 12:35:04 +0530

    data types.py

---
+++ DAY 2/data types.py
@@ -0,0 +1,35 @@
+# Strings - Characters inside a quotes
+# Printing Each Letters from a string using indexing this process is also known as Subscripting
+print("Hello"[0])
+#Using Positive Indexing to Print Each Letters from a string indexing starts with zero
+print("Hello"[1])
+print("Hello"[2])
+print("Hello"[3])
+print("Hello"[4])
+# Using Negative Indexing to print each letters from a string it starts with 1
+print("Hello"[-1])
+print("Hello"[-2])
+print("Hello"[-3])
+print("Hello"[-4])
+print("Hello"[-5])
+
+variable1="Tom"
+print(variable1[0])
+print(variable1[1])
+print(variable1[2])
+
+variable1="Tom"
+print(variable1[-1])
+print(variable1[-2])
+print(variable1[-3])
+
+# Integers - Whole Numbers
+a=12
+b=int(1)
+print(a)
+print(b)
+
+
+
+
+
